60a069f231ccc9b5d481d753ba8c685e4c56419d
D3auuvc <chenghan.chung@ut.ee>
2022-04-04 14:25:07 +0300

    [DS] Modified mini project server side

--- 11_DS/00_Mini_Project_1/rpyc_server.py
+++ 11_DS/00_Mini_Project_1/rpyc_server.py
@@ -1,5 +1,4 @@
 from random import randint
-from operator import itemgetter
 import rpyc
 from rpyc.utils.server import ThreadedServer
 import datetime
@@ -8,12 +7,9 @@ import threading
 from multiprocessing import Process
 import _thread
 
-
 # global variables
 system_start = datetime.datetime.now()
 date_time = datetime.datetime.now()
-nodes = []
-cs_q = []
 
 
 class Message(object):
@@ -24,7 +20,7 @@ class Message(object):
 
 class Process(threading.Thread):
 
-    def __init__(self, id):
+    def __init__(self, id, threadLock, nodes):
         super(Process, self).__init__()
         self.id = id
         self.timestamp = 0
@@ -34,26 +30,27 @@ class Process(threading.Thread):
         self.message = Message()
         self.inTheQ = False
         self.recvAck = []
+        self.waitForResponse = []
+        self.mutex = threadLock
+        self.nodes = nodes
 
     def _send_ack(self, message):
-        global nodes
-
-        recever = next((x for x in nodes if x.id == message.id), None)
+        recever = next((x for x in self.nodes if x.id == message.id), None)
         recever.recvAck.append({'sender': self.id, 'msg': 'OK'})
         recever.timestamp = max(recever.timestamp, self.timestamp)+1
         print(f'node {self.id} send OK to {recever.id}')
 
-    def _release_cs(self, next_node):
-        global nodes
+    def _release_cs(self):
         self.status = 'DO-NOT-WANT'
-        if next_node != None:
-            recever = next((x for x in nodes if x.id == next_node.id), None)
-            recever.recvAck.append({'sender': self.id, 'msg': 'OK'})
-            recever.timestamp = max(recever.timestamp, self.timestamp)+1
-            print(f'node {self.id} release CS and give it to {recever.id}')
-            self.timestamp += 1
+        self.mutex.release()
+        if len(self.waitForResponse) != 0:
+            for wn in self.waitForResponse:
+                self.timestamp += 1
+                recever = next((x for x in self.nodes if x.id == wn.id), None)
+                recever.recvAck.append({'sender': self.id, 'msg': 'OK'})
+                recever.timestamp = max(recever.timestamp, self.timestamp)+1                
+            self.waitForResponse = []
         else:
-            print('No HOLDER')
             self.timestamp += 1
 
     def handel_receive_message(self, message):
@@ -64,52 +61,52 @@ class Process(threading.Thread):
             if self.timestamp == message.timestamp:
                 if self.id > message.id:
                     self._send_ack(message)
+                else:
+                    self.waitForResponse.append(message)
             elif self.timestamp > message.timestamp:
                 self._send_ack(message)
             else:
-                pass
+                self.waitForResponse.append(message)
         elif self.status == 'HELD':
-            pass
+            self.waitForResponse.append(message)
+
+    # Simulate sokect function
+    def _send_message(self, message):
+        for n in self.nodes:
+            if n.id == message.id:
+                pass
+            else:
+                n.handel_receive_message(message)
 
     def _check_status(self):
-        global nodes
-        global cs
-        global cs_q
-
         if self.status == 'DO-NOT-WANT':
             time.sleep(self.pt)
             self.status = 'WANTED'
             self.timestamp += 1
         elif self.status == 'WANTED':
-            if self.inTheQ is False:
-                self.message = Message(self.id, self.timestamp)
+            if self.inTheQ is False:                
                 self.inTheQ = True
-                print(
-                    f'node {self.id} send the timestamp: {self.message.timestamp}')
-                send_message(self.message)
-            if len(self.recvAck) == (len(nodes)-1):
-                remove = cs_q.pop(0)
-                print(f'node {self.id} remove {remove.id}')
-                for q in cs_q:
-                    print(q.id,q.timestamp)
+                print(f'node {self.id} send the timestamp: {self.message.timestamp}')
+                self.timestamp += 1
+                self.message = Message(self.id, self.timestamp)
+                self._send_message(self.message)
+            if len(self.recvAck) == (len(self.nodes)-1):
                 self.inTheQ = False
                 self.recvAck = []
+                self.mutex.acquire()
                 self.status = 'HELD'
                 self.timestamp += 1
         elif self.status == 'HELD':
+            for n in self.nodes:
+                print(f'{n.id}, {n.status}, {n.recvAck}')
             time.sleep(self.cst)
-            if len(cs_q) == 0:
-                self._release_cs(None)
-            else:
-                self._release_cs(cs_q[0])
+            self._release_cs()
 
     # starts a thread that runs the process
     def start(self):
         _thread.start_new_thread(self.run, ())
 
     def run(self):
-        global q
-
         while True:
             t = threading.Thread(target=self._check_status())
             t.daemon = True
@@ -117,46 +114,31 @@ class Process(threading.Thread):
             t.join()
 
 
-# Simulate sokect function
-def send_message(message):
-    global cs_q
-    global nodes
-
-    cs_q.append(message)
-    cs_q.sort(key=lambda x: (x.timestamp,x.id))
-    for n in nodes:
-        if n.id==message.id:
-            pass
-        else:
-            n.handel_receive_message(message)
-
-
 class MonitorService(rpyc.Service):
-    def exposed_run(self, num_nodes):
-        global nodes
+    def __init__(self):
+        self.nodes = []
+        self.threadLock = threading.Lock()
 
+    def exposed_run(self, num_nodes):
         for i in range(int(num_nodes)):
-            nodes.append(Process(i+1))
+            self.nodes.append(Process(i+1, self.threadLock, self.nodes))
 
         # Activate process
-        for n in nodes:
+        for n in self.nodes:
             n.name = str(n.id)
             n.daemon = True
             n.start()
 
     def exposed_update_cst(self, t):
-        global nodes
-        for n in nodes:
+        for n in self.nodes:
             n.cst = randint(10, t)
 
     def exposed_update_pt(self, t):
-        global nodes
-        for n in nodes:
+        for n in self.nodes:
             n.pt = randint(5, t)
 
     def exposed_show_list(self):
-        global nodes
-        for n in nodes:
+        for n in self.nodes:
             print(f'{n.id}, {n.status}')
 
 
